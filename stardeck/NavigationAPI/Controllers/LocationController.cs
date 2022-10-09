3c9acf2f2c19b8901301a1ff242cc799b2333a66
AutMai <shuber0802@gmail.com>
2022-10-09 13:03:27 +0200

    added methods in locationController to cast to specific derived type

--- stardeck/NavigationAPI/Controllers/LocationController.cs
+++ stardeck/NavigationAPI/Controllers/LocationController.cs
@@ -15,10 +15,23 @@ public class LocationController : AController<Location, CreateLocationDto, Locat
     public LocationController(IRepository<Location> repo) : base(repo) {
     }
 
-    [HttpGet("{name}")]
-    public async Task<ActionResult<PlanetDto>> GetLocation(string name) {
-        var res = (await _repo.ReadAsync(l => l.Name == name)).SingleOrDefault();
+    public override async Task<ActionResult<LocationDto>> ReadAsync(int id) {
+        var res = await _repo.ReadAsync(id);
+
         return res switch {
+            null => NotFound(),
+            Planet p => Ok(p.Adapt<PlanetDto>()),
+            Galaxy g => Ok(g.Adapt<GalaxyDto>()),
+            _ => Ok(res)
+        };
+    }
+
+    [HttpGet("{name}")]
+    public async Task<ActionResult<LocationDto>> ReadByNameAsync(string name) {
+        var res = (await _repo.ReadAsync(l => l.Name == name)).SingleOrDefault();
+
+        return res switch {
+            null => NotFound(),
             Planet p => Ok(p.Adapt<PlanetDto>()),
             Galaxy g => Ok(g.Adapt<GalaxyDto>()),
             _ => Ok(res)
