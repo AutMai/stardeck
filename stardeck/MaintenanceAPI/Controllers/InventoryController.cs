d0d48668e6121a57789f37fcc474392cc240312c
BefDev <92431971+BefDev@users.noreply.github.com>
2022-10-13 19:33:54 +0200

    added InventoryController.cs

---
+++ stardeck/MaintenanceAPI/Controllers/InventoryController.cs
@@ -0,0 +1,16 @@
+﻿using MaintenanceDomain.Repositories.Interfaces;
+using MaintenanceDTO.Create;
+using MaintenanceDTO.Read;
+using MaintenanceModel.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaintenanceAPI.Controllers;
+
+[ApiController]
+[Route("/maintenance/inventory")]
+public class InventoryController : AController<Inventory, CreateInventoryDTO, InventoryDTO>
+{
+    public InventoryController(IRepository<Inventory> repo) : base(repo)
+    {
+    }
+}
