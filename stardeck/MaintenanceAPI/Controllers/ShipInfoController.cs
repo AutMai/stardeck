0d681c87a13742341828da265bf21177190c2f79
Sebastian <shuber0802@gmail.com>
2022-10-20 08:03:42 +0200

    added eventbus connection

--- stardeck/MaintenanceAPI/Controllers/ShipInfoController.cs
+++ stardeck/MaintenanceAPI/Controllers/ShipInfoController.cs
@@ -1,4 +1,6 @@
-﻿using MaintenanceDomain.Repositories.Interfaces;
+﻿using System.Text;
+using EventBusConnection;
+using MaintenanceDomain.Repositories.Interfaces;
 using MaintenanceDTO.Create;
 using MaintenanceDTO.Read;
 using MaintenanceModel.Entities;
@@ -11,5 +13,20 @@ namespace MaintenanceAPI.Controllers;
 public class ShipInfoController : AController<ShipInfo, CreateShipInfoDTO, ShipInfoDTO> {
     public ShipInfoController(IRepository<ShipInfo> repo) : base(repo)
     {
+    }
+    
+    [HttpGet("/test")]
+    public async Task<ActionResult<IEnumerable<ShipInfoDTO>>> Get() {
+        var c = new Connector();
+        c.Init();
+        var model = c.GetModel();
+        
+        var properties = model.CreateBasicProperties();
+        properties.Persistent = false;
+        
+        byte[] messagebuffer = Encoding.Default.GetBytes("Direct Message");
+
+        model.BasicPublish("stardeckExchange", "", false, properties, messagebuffer);
+        return Ok(await _repo.ReadAsync(1));
     }
 }
