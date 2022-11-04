cc20a6d38bd8b0488315f84b467a5ed73aaed36c
BefDev <92431971+BefDev@users.noreply.github.com>
2022-11-04 16:26:19 +0100

    added IntrudersDetectedEvent.cs

---
+++ stardeck/EventBusConnection/EventsProcessing/Events/IntrudersDetectedEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace EventBusConnection.EventsProcessing.Events;
+
+public class IntrudersDetectedEvent : BaseEvent
+{
+    public IntrudersDetectedEvent()
+    {
+        
+    }
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
