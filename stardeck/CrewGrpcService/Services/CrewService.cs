1f74eeb0d4bb8d97caaf20eadffd721ecdf5348e
BefDev <92431971+BefDev@users.noreply.github.com>
2022-11-04 16:26:56 +0100

    git pfusch

--- stardeck/CrewGrpcService/Services/CrewService.cs
+++ stardeck/CrewGrpcService/Services/CrewService.cs
@@ -1,37 +1,62 @@
 using Crew;
 using Grpc.Core;
 using CrewGrpcService.Datastore;
+using CrewModel.Entities;
 using Google.Protobuf.WellKnownTypes;
 
 namespace CrewGrpcService.Services;
 
-public class CrewService : Crew.Crew.CrewBase {
+public class CrewService : Crew.Crew.CrewBase
+{
     private readonly ILogger<CrewService> _logger;
 
-    public CrewService(ILogger<CrewService> logger) {
+    public CrewService(ILogger<CrewService> logger)
+    {
         _logger = logger;
     }
 
-    public override Task<CrewReplies> ReadCrew(Empty request, ServerCallContext context) {
+    public override Task<CrewReplies> ReadCrew(Empty request, ServerCallContext context)
+    {
         var crewReplies = new CrewReplies();
         crewReplies.Crew.AddRange(CrewDatastore.Crew.Select(c => c.ToCrewReply()));
         return Task.FromResult(crewReplies);
     }
 
-    public override Task<CrewReply> ReadSingleCrewMember(SingleCrewMemberRequest request, ServerCallContext context) {
+    public override Task<CrewReply> ReadSingleCrewMember(SingleCrewMemberRequest request, ServerCallContext context)
+    {
         var x = CrewDatastore.Crew.SingleOrDefault(c => c.CrewMemberId == request.CrewMemberId);
         return Task.FromResult(x.ToCrewReply());
     }
 
-    public override async Task ReadLogbook(Empty request, IServerStreamWriter<LogbookReply> responseStream, ServerCallContext context) {
-        foreach (var logbook in CrewDatastore.Logbook) {
+    public override async Task ReadLogbook(Empty request, IServerStreamWriter<LogbookReply> responseStream,
+        ServerCallContext context)
+    {
+        foreach (var logbook in CrewDatastore.Logbook)
+        {
             await responseStream.WriteAsync(logbook.ToLogbookReply());
         }
     }
 
     public override Task<LogbookReply> ReadSingleEntryLogbook(SingleLogbookEntryRequest request,
-        ServerCallContext context) {
+        ServerCallContext context)
+    {
         var x = CrewDatastore.Logbook.SingleOrDefault(l => l.LogId == request.Id);
         return Task.FromResult(x.ToLogbookReply());
+    }
+
+    public override Task<LogbookReply> CreateLogbookEntry(LogbookEntryRequest request, ServerCallContext context)
+    {
+        var id = CrewDatastore.Logbook.Max(l => l.LogId) + 1;
+        Logbook l;
+        CrewDatastore.Logbook.Add(l = new Logbook()
+        {
+            LogId = id,
+            Content = request.Content,
+            Title = request.Title,
+            AuthorMember = CrewDatastore.Crew.SingleOrDefault(am => am.CrewMemberId == request.AuthorId),
+            AuthorMemberId = request.AuthorId
+        });
+
+        return Task.FromResult(l.ToLogbookReply());
     }
 }
